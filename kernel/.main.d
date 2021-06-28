ce5d432abfd886eff6d81d2a1f38f7a575b4b6ed
shintard <flekystyley@gmail.com>
2021-06-28 20:26:14 +0900

    added console;

---
+++ kernel/.main.d
@@ -0,0 +1,9 @@
+main.o: main.cpp \
+  /home/b0bd11an/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/b0bd11an/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/b0bd11an/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/b0bd11an/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/b0bd11an/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/b0bd11an/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
+  /home/b0bd11an/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
+  frame_buffer_config.hpp graphics.hpp font.hpp console.hpp
